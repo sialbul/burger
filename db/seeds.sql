b13c764291fdc21943eaedba60cb3f46e0e933d4
Sibel <sbaslamisli@yahoo.com>
2019-04-29 00:49:22 -0400

    first setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('HamBurger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BeefBurger', true);
+INSERT INTO burgers (burger_name) VALUES ('VeggieBurger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,44 @@
+$(function() {
+    $(".change-devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var newBurger = $(this).data("newburger");
+  
+      var newBurgerState = {
+        devoured: newBurger
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newBurgerState
+      }).then(
+        function() {
+          console.log("changed devoured to", newBurger);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
