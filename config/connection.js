5b826a37e2233b6ca16f11bb5a872873740f5490
Sibel <sbaslamisli@yahoo.com>
2019-05-01 15:51:28 -0400

    connection update

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,17 @@
-// Set up MySQL connection.
-
 var mysql = require("mysql");
 var connection;
-​
+
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Efemir.2010",
-    database: "burgers_db"
-  });
+    connection = mysql.createConnection({
+        database: "burgers_db",
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Efemir.2010"
+    });
 }
-​
+
 connection.connect();
 module.exports = connection;
-
