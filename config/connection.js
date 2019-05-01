698547b2adfa6d81206f1ed4aedf7884c25c8152
Sibel <sbaslamisli@yahoo.com>
2019-05-01 18:15:54 -0400

    update port

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         database: "burgers_db",
         host: "localhost",
-        port: 3306,
+        port: 8080,
         user: "root",
         password: "Efemir.2010"
     });
