672a9f4326cb055c2cbaf62194d37def458f794f
Sibel Baslamisli <sbaslamisli@yahoo.com>
2021-10-26 21:44:51 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "Efemir.2010"
+    password: process.env.REACT_APP_PASS,
     });
 }
 
